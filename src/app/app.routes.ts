6daab08cbcb0aabf8de13c24c4b27e1137b99ee3
icet-sharadha <icet.sharadha@gmail.com>
2024-11-10 09:12:24 +0530

    create nav and dash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppointmentManageComponent } from './page/appointment-manage/appointment-manage.component';
 import { PatientManageComponent } from './page/patient-manage/patient-manage.component';
+import { LoginComponent } from './page/login/login.component';
+import { DashBordPageComponent } from './page/dash-bord-page/dash-bord-page.component';
 
 export const routes: Routes = [
+    {
+        path:'',
+        component:LoginComponent
+    },
     {
         path:"appointment",
         component:AppointmentManageComponent
@@ -10,5 +16,9 @@ export const routes: Routes = [
     {
         path:"patient",
         component:PatientManageComponent
+    },
+    {
+        path:"dashbord",
+        component:DashBordPageComponent
     }
 ];
---
+++ src/app/page/dash-bord-page/dash-bord-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavComponent } from "../../common/nav/nav.component";
+
+@Component({
+  selector: 'app-dash-bord-page',
+  standalone: true,
+  imports: [NavComponent],
+  templateUrl: './dash-bord-page.component.html',
+  styleUrl: './dash-bord-page.component.css'
+})
+export class DashBordPageComponent {
+
+}
