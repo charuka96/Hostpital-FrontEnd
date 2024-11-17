414ca3955548466512bf402ad3cdf59811db80fd
icet-sharadha <icet.sharadha@gmail.com>
2024-11-17 15:23:09 +0530

    V2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,9 @@ import { PatientRootComponent } from './page/patient-root/patient-root.component
 import { DashPageComponent } from './page/dash-page/dash-page.component';
 import { AddAppointmentComponent } from './page/add-appointment/add-appointment.component';
 import { AppoimtmentRootComponent } from './page/appoimtment-root/appoimtment-root.component';
+import { ReportRootComponent } from './page/report-root/report-root.component';
+import { AddReportComponent } from './page/add-report/add-report.component';
+import { ManageReportComponent } from './page/manage-report/manage-report.component';
 
 export const routes: Routes = [
   {
@@ -66,6 +69,24 @@ export const routes: Routes = [
           }
         ]
       },
+      {
+        path:"report",
+        component:ReportRootComponent,
+        children:[
+          {
+            path:"",
+            component:ManageReportComponent
+          },
+          {
+            path:"add-report",
+            component:AddReportComponent
+          },
+          {
+            path:"view-all",
+            component:ManageReportComponent
+          }
+        ]
+      }
 
     ]
   }
