72ddf4996051ddd1eff7030397b867c030f05f7d
charuka96 <chaukathisaru96@gmail.com>
2025-03-01 12:50:11 +0530

    first commit

--- src/app/page/patient-manage/patient-manage.component.ts
+++ src/app/page/patient-manage/patient-manage.component.ts
@@ -34,74 +34,19 @@ export class PatientManageComponent {
   }
 
   deletePatient(id: any) {
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: "btn btn-success",
-        cancelButton: "btn btn-danger"
-      },
-      buttonsStyling: false
-    });
-    swalWithBootstrapButtons.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Yes, delete it!",
-      cancelButtonText: "No, cancel!",
-      reverseButtons: true
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.http.delete(`http://localhost:8080/patient/delete-by-id/${id}`).subscribe(data => {
-          this.loadPatient();
-        })
-        swalWithBootstrapButtons.fire({
-          title: "Deleted!",
-          text: "Your file has been deleted.",
-          icon: "success"
-        });
 
-
-      } else if (
-        /* Read more about handling dismissals below */
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-        swalWithBootstrapButtons.fire({
-          title: "Cancelled",
-          text: "Your imaginary file is safe :)",
-          icon: "error"
-        });
-      }
-    });
-
-  }
-
+this.http.delete(`http://localhost:8080/patient/delete-by-id/${id}`).subscribe( data => {
+alert("Patient deleted")
+this.patientList=this.patientList.filter(_=>_.id != id)})
+}
   public selectedPatient: any = {};
-
   selectPatient(patient: any) {
     console.log(patient);
-
     this.selectedPatient = patient;
 
   }
 
   updatePatient() {
-    Swal.fire({
-      title: "Do you want to save the changes?",
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: "Save",
-      denyButtonText: `Don't save`
-    }).then((result) => {
-      /* Read more about isConfirmed, isDenied below */
-      if (result.isConfirmed) {
         this.http.put("http://localhost:8080/patient/update", this.selectedPatient).subscribe(res => {
-          Swal.fire("Saved!", "", "success");
-        })
-
-      } else if (result.isDenied) {
-        Swal.fire("Changes are not saved", "", "info");
-      }
-    });
-
-  }
-}
+          console.log(res);
+        })}}
