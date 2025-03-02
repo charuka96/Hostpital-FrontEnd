8afea36d766b2513d4deced5601c83877862b25f
charuka96 <chaukathisaru96@gmail.com>
2025-03-02 20:23:58 +0530

    first commit

--- src/app/model/MedicalReport.ts
+++ src/app/model/MedicalReport.ts
@@ -1,18 +1,47 @@
+import { Form } from "@angular/forms";
+
 export class MedicalReport {
     public id: number = 0;
-    public category: string;
     public pdfSrc: string;
     public localDateTime: string;
     public patientId: number;
-    public adminId: number;
-    public labNumber: number;
+    public file :File =new File(["foo"],"foo.tex",{
+      type:"text/plain",
+    }) ;
 
-    constructor(category: string, pdfSrc: string, localDateTime: string, patientId: number, adminId: number, labNumber: number) {
-        this.category = category;
+    constructor( pdfSrc: string, localDateTime: string, patientId: number) {
         this.pdfSrc = pdfSrc;
         this.localDateTime = localDateTime;
         this.patientId = patientId;
-        this.adminId = adminId;
-        this.labNumber = labNumber;
+
     }
+
+public setpdfSrc(pdfSrc:string){
+  this.pdfSrc = pdfSrc;
 }
+
+public setlocalDateTime(localDateTime:string){
+  this.localDateTime = localDateTime;
+}
+
+public setPatientId(patientId:number){
+ this.patientId = patientId;
+}
+
+public setfile(file:File){
+  this.file = file;
+}
+
+public getId():number{
+  return this.id;
+}
+
+public getpdfSrc():string{
+  return this.pdfSrc;
+}
+public getPatientId():number{
+return this.patientId;}
+
+public getFile():File{
+  return this.file;
+}}
--- src/app/page/add-report/add-report.component.ts
+++ src/app/page/add-report/add-report.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MedicalReport } from '../../model/MedicalReport';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-report',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-report.component.html',
   styleUrl: './add-report.component.css'
 })
 export class AddReportComponent {
+
+ public formDate:FormData= new FormData();
+ public report:MedicalReport= new MedicalReport("", "", 0);
+ selectedFile:File|null = null;
+
+  onFileSelected(event:Event){
+    const inputElement =event.target as HTMLInputElement;
+    if(inputElement.files && inputElement.files.length>0){
+      this.selectedFile=inputElement.files[0];
+      console.log(this.selectedFile);
+            this.report.setfile(this.selectedFile);
+    }
+  }
+
+  constructor(private http:HttpClient){}
+
+postObj:any={
+ report:this.report,
+ file:this.selectedFile
+}
+  addReport():void{
+
+  console.log(this.postObj);
+  this.http.post("http://localhost:8080/medical-report/add-report",this.postObj).subscribe(res=>{
+    console.log("report added successfully");
+})
+
+  }
 
 }
